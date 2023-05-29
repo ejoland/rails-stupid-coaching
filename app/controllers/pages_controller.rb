4acd60df9ec630d53bb4c95e68568d1e484944cd
enrico joland <enrico.joland@gmail.com>
2023-05-29 13:52:59 +0200

    add bootstrap

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,12 +8,12 @@ class PagesController < ApplicationController
   end
 
   def answer
-    question = params[:question]
+    @question = params[:question]
 
-    @answer = if question.present?
-                if question == 'I am going to work'
+    @answer = if @question.present?
+                if @question == 'I am going to work'
                   'Great!'
-                elsif question.ends_with?('?')
+                elsif @question.ends_with?('?')
                   'Silly question, get dressed and go to work!'
                 else
                   "I don't care, get dressed and go to work!"
